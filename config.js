2040ba1552e27fd5138946c69e58b43faeb06d3e
Alexandros Papadopoulos <al.pap@live.com>
2022-04-09 18:28:28 +0200

    new

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 export default {
-  enable_slack: true,
-  enable_discord: false,
-  discord_webhook_id: '',
-  discord_webhook_token: '',
-  slack_webhook_url: '',
-  slack_channel: '#statistics',
-  slack_username: 'Statistics',
+  slack_webhook_url: 'https://hooks.slack.com/services/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',
+  slack_channel: '#test',
+  slack_username: 'Skroutz',
   products: [
     {
-      name: 'AMD Ryzen 5 5600X',
-      url: 'https://www.skroutz.gr/s/25549852/AMD-Ryzen-5-5600X-Box.html',
+      name: 'Samsung Galaxy A52s 5G',
+      url: 'https://www.skroutz.gr/s/30662995/Samsung-Galaxy-A52s-5G-6GB-128GB-Awesome-Black.html',
       lower_then_threshold: 350,
+    },
+    {
+      name: 'Apple iPhone 11 ',
+      url: 'https://www.skroutz.gr/s/20060269/Apple-iPhone-11-4GB-64GB-Black.html',
+      lower_then_threshold: 950,
     },
   ],
   check_every_seconds: 3600,
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const fs = require('fs')
-const cheerio = require('cheerio')
-const axios = require('axios')
-const chalk = require('chalk')
-const moment = require('moment')
-const Discord = require('discord.js')
-const SlackNotify = require('slack-notify')
-const settings = require('./config.js')
+import fs from 'fs'
+import cheerio from 'cheerio'
+import axios from 'axios'
+import chalk from 'chalk'
+import moment from 'moment'
+import Discord from 'discord.js'
+import SlackNotify from 'slack-notify'
+import settings from './config.js'
 
 // A message will be sent to this webhook when the product reaches the threshold price or lower
 // The id is the first part code of the URL you get when creating a webhook in a channel
@@ -20,10 +20,7 @@ var slack_notify = slack.extend({
 
 const webhook = new Discord.WebhookClient(settings.discord_webhook_id, settings.discord_webhook_token)
 
-// How many seconds to wait before checking availability
-// Set it atleast 5 as to not get IP banned since i haven't implemented proxies
-
-const interval = seconds * settings.check_every_seconds
+const interval = 1000 * settings.check_every_seconds
 
 // The name of the log file
 const logFile = 'log.txt'
@@ -44,26 +41,29 @@ const checkSkroutz = (url, name, threshold) => {
       const $ = cheerio.load(response.data)
 
       let prods = []
-      $('.js-product-link.product-link.content-placeholder').each((i, elem) => {
-        prods.push([elem.children[0].data, elem.attribs.href])
+      $('.dominant-price').each((i, elem) => {
+        prods.push(elem.children[0].data.split(' ')[0])
       })
-
       if (prods.length > 0) {
-        console.log(`[${moment().format('HH:mm:ss')}] ${chalk.green('info')} :: [skroutz] [x${prods.length}] [${productName}] :: ${chalk.green('IN STOCK')}`)
-        fs.appendFileSync(logFile, `[${moment().format('HH:mm:ss')}] info:: [skroutz] [x${prods.length}] [${productName}] :: IN STOCK\n`)
+        console.log(`[${moment().format('HH:mm:ss')}] ${chalk.green('info')} :: [skroutz] [x${prods.length}] [${name}] :: ${chalk.green('IN STOCK')}`)
+        fs.appendFileSync(logFile, `[${moment().format('HH:mm:ss')}] info:: [skroutz] [x${prods.length}] [${name}] :: IN STOCK\n`)
       } else {
-        console.log(`[${moment().format('HH:mm:ss')}] ${chalk.green('info')} :: [skroutz] [x0] [${productName}] :: ${chalk.red('OUT OF STOCK')}`)
-        fs.appendFileSync(logFile, `[${moment().format('HH:mm:ss')}] info :: [skroutz] [x0] [${productName}] :: OUT OF STOCK\n`)
+        console.log(`[${moment().format('HH:mm:ss')}] ${chalk.green('info')} :: [skroutz] [x0] [${name}] :: ${chalk.red('OUT OF STOCK')}`)
+        fs.appendFileSync(logFile, `[${moment().format('HH:mm:ss')}] info :: [skroutz] [x0] [${name}] :: OUT OF STOCK\n`)
       }
 
-      lowest = parseInt(prods[0][0].substring(0, prods[0][0].length - 1).replace(',', '.'))
-
+      const lowest = parseInt(prods.sort()[0])
       if (lowest <= threshold) {
-        console.log(`[${moment().format('HH:mm:ss')}] ${chalk.green('info')} :: [skroutz] [${productName}] :: ${chalk.green('REACHED THRESHOLD')} [${lowest}€]`)
+        console.log(`[${moment().format('HH:mm:ss')}] ${chalk.green('info')} :: [skroutz] [${name}] :: ${chalk.green('REACHED THRESHOLD')} [${lowest}€]`)
         console.log(`[${moment().format('HH:mm:ss')}] ${chalk.green('info')} :: ${chalk.blue('SENDING WEBHOOK...')}`)
-        const data = `${name} is in stock at https://www.skroutz.gr${prods[0][1]} for ${lowest}€`
-        if (settings.enable_slack) slack_notify({ text: data }).catch(console.error)
-        if (settings.enable_discord) webhook.send(data).catch(console.error)
+        const data = `${name} ${lowest}€ is in stock at: \n ${url}`
+        slack_notify({
+          text: name,
+          fields: {
+            value: `${lowest}€`,
+            url: url,
+          },
+        }).catch(console.error)
       }
     })
     .catch((error) => {
@@ -71,9 +71,16 @@ const checkSkroutz = (url, name, threshold) => {
     })
 }
 
+settings.products.map(async (product) => {
+  await sleep(5000) // How many seconds to wait before checking availability
+  // Set it atleast 5 as to not get IP banned since i haven't implemented proxies
+  checkSkroutz(product.url, product.name, product.lower_then_threshold)
+})
+
 setInterval(() => {
-  settings.products.map((product) => {
-    sleep(5000)
+  for (const product in settings.products) {
+    sleep(5000).then() // How many seconds to wait before checking availability
+    // Set it atleast 5 as to not get IP banned since i haven't implemented proxies
     checkSkroutz(product.url, product.name, product.lower_then_threshold)
-  })
+  }
 }, interval)
