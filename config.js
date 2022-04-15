e18e77f0bdcb62bee2f00501938bf7a38caa7cac
Alexandros Papadopoulos <al.pap@live.com>
2022-04-15 20:57:59 +0200

    update

--- config.js
+++ config.js
@@ -13,6 +13,11 @@ export default {
       url: 'https://www.skroutz.gr/s/20060269/Apple-iPhone-11-4GB-64GB-Black.html',
       lower_then_threshold: 950,
     },
+    {
+      name: 'Apple iPhone 11 ',
+      url: 'https://www.skroutz.gr/s/20060269/Apple-iPhone-11-4GB-64GB-Black.html',
+      lower_then_threshold: 950,
+    },
   ],
   check_every_seconds: 3600,
 }
